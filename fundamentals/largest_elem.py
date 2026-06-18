6b588a9a5b216d5c54b6a309affeba0ee8b669a2
Akshay M Nair <akshaymanikuttan05@gmail.com>
2026-06-18 12:15:50 +0000

    Largest element using python

---
+++ fundamentals/largest_elem.py
@@ -0,0 +1,19 @@
+class Solution:
+    def largest_element(self,arr):
+        if len(arr) >= 2:
+            temp = 0
+            for i in range(len(arr)):
+                if (temp <= arr[i]):
+                    temp = arr[i]
+            return temp
+
+        elif len(arr) == 1:
+            temp = arr
+            return int(temp[0])
+
+        else:
+            return None
+
+sol = Solution()
+largest = sol.largest_element([10,1,2,3])
+print(largest)
