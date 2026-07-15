616071862ab1e8fecebf094dbdbcb2149588c865
Akshay M Nair <akshaymanikuttan05@gmail.com>
2026-07-15 09:07:07 +0000

    fib and count problem and dp implementation using python

--- neetcode/dup_count.py
+++ neetcode/dup_count.py
@@ -1,9 +1,6 @@
-class Solution(object):
-    def findDuplicate(self, nums):
-        counts = Counter(nums)
-        for key,count in counts.items():
-            if count > 1:
-                value = key
-            else:
-                continue
-        return value
+def climbStairs(self, n: int) -> int:
+        if n <= 2: return n
+        dp = [0]*(n+1)
+        dp[1],dp[2] = 1,2
+        for i in range(3,n+1): dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
