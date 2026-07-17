71142bda5d50bb450a840f1f75036e3fbc258d71
Akshay M Nair <akshaymanikuttan05@gmail.com>
2026-07-17 16:41:47 +0000

    added permutations

---
+++ neetcode/backtracking/permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def backtrack(path):
+            #base case where length of the path and length of the nums become same 
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+                
+            #constrains
+            for num in nums:
+                if num in path:
+                    continue 
+                path.append(num)
+                backtrack(path)
+                path.pop()
+
+        backtrack([])
+        return result
